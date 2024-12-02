cf24bf57aba7fe9fbe69ba498536e08050efdf2d
Artemy Kaydash <artemy@kayart.dev>
2024-12-02 17:34:42 +0200

    Added API key validation and error notification to the settings page

--- getanewsletter.php
+++ getanewsletter.php
@@ -645,6 +645,7 @@ function display_api_key_form() {
 function newsletter_options() {
     $news_pass = get_option('newsletter_pass');
     $ok = false;
+    $is_api_token_correct = false;
 
     if ( $news_pass ) {
         $conn = new GAPI( '', $news_pass );
@@ -654,9 +655,8 @@ function newsletter_options() {
         return;
     }
 
-    if ( ! $ok ) {
-        display_api_key_form();
-        return;
+    if ( $ok ) {
+        $is_api_token_correct = true;
     }
 ?>
     <div class="wrap gan-settings-page">
@@ -679,12 +679,18 @@ function newsletter_options() {
                     <input type="password" name="newsletter_pass" id="newsletter_pass" value="<?php echo get_option('newsletter_pass'); ?>" />
 
                     <div class="gan-result-message">
-                        <div class="gan-success-message">
-                            <div class="gan-checkmark-container">
-                                <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="50px" height="50px"><path d="M 41.9375 8.625 C 41.273438 8.648438 40.664063 9 40.3125 9.5625 L 21.5 38.34375 L 9.3125 27.8125 C 8.789063 27.269531 8.003906 27.066406 7.28125 27.292969 C 6.5625 27.515625 6.027344 28.125 5.902344 28.867188 C 5.777344 29.613281 6.078125 30.363281 6.6875 30.8125 L 20.625 42.875 C 21.0625 43.246094 21.640625 43.410156 22.207031 43.328125 C 22.777344 43.242188 23.28125 42.917969 23.59375 42.4375 L 43.6875 11.75 C 44.117188 11.121094 44.152344 10.308594 43.78125 9.644531 C 43.410156 8.984375 42.695313 8.589844 41.9375 8.625 Z"/></svg>
+                        <?php if ( $is_api_token_correct ): ?>
+                            <div class="gan-success-message">
+                                <div class="gan-checkmark-container">
+                                    <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="50px" height="50px"><path d="M 41.9375 8.625 C 41.273438 8.648438 40.664063 9 40.3125 9.5625 L 21.5 38.34375 L 9.3125 27.8125 C 8.789063 27.269531 8.003906 27.066406 7.28125 27.292969 C 6.5625 27.515625 6.027344 28.125 5.902344 28.867188 C 5.777344 29.613281 6.078125 30.363281 6.6875 30.8125 L 20.625 42.875 C 21.0625 43.246094 21.640625 43.410156 22.207031 43.328125 C 22.777344 43.242188 23.28125 42.917969 23.59375 42.4375 L 43.6875 11.75 C 44.117188 11.121094 44.152344 10.308594 43.78125 9.644531 C 43.410156 8.984375 42.695313 8.589844 41.9375 8.625 Z"/></svg>
+                                </div>
+                                <span>Your API token is active and working</span>
                             </div>
-                            <span>Your API token is active and working</span>
-                        </div>
+                        <?php else:  ?>
+                            <div class="notice notice-error inline">
+                                <p>Please, double check if the provided API key is correct</p>
+                            </div>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
