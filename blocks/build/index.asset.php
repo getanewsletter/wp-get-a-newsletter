8d940e21486943af1dd0f5d741861e657c57df62
simonwallstrom <simon.wallstrom@gmail.com>
2025-01-15 12:54:28 +0100

    Updated all npm packages to the latest version and added better validation for the block forms

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '911715d52bfffd0ac8fa');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3861ddd816410afc7fff');
